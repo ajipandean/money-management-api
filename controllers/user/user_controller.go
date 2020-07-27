6cde8af71eb07243b7a277af0e5aa038b14ea0ba
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-07-27 21:43:29 +0800

    Complete user controller logic

--- controllers/user/user_controller.go
+++ controllers/user/user_controller.go
@@ -3,20 +3,79 @@ package user_controller
 import (
     "net/http"
     "github.com/labstack/echo/v4"
+    d "money-management-api/database"
+    m "money-management-api/models"
 )
 
+type M map[string]interface{}
+
 func FetchUsers(c echo.Context) error {
-    return c.String(http.StatusOK, "Users fetched")
+    db := d.DB
+    users := new([]m.User)
+
+    db.Find(&users)
+
+    return c.JSON(http.StatusOK, users)
 }
+
 func FindUser(c echo.Context) error {
-    return c.String(http.StatusOK, "Users found")
+    db := d.DB
+    user := new(m.User)
+
+    if res := db.First(user, c.Param("id")); res.RecordNotFound() {
+        return c.JSON(http.StatusInternalServerError, M{
+            "Message": "User not found",
+        })
+    }
+
+    return c.JSON(http.StatusOK, user)
 }
+
 func CreateUser(c echo.Context) error {
-    return c.String(http.StatusOK, "Users created")
+    db := d.DB
+    user := new(m.User)
+
+    if err := c.Bind(user); err != nil {
+        panic(err.Error())
+    }
+
+    if res := db.Create(user); res.Error != nil {
+        return c.JSON(http.StatusInternalServerError, res.Error)
+    }
+
+    return c.JSON(http.StatusOK, user)
 }
+
 func UpdateUser(c echo.Context) error {
-    return c.String(http.StatusOK, "Users updated")
+    db := d.DB
+    user := new(m.User)
+
+    if res := db.First(user, c.Param("id")); res.RecordNotFound() {
+        return c.JSON(http.StatusInternalServerError, M{
+            "Message": "User not found",
+        })
+    }
+
+    if err := c.Bind(user); err != nil {
+        panic(err.Error())
+    }
+
+    db.Save(user);
+
+    return c.JSON(http.StatusOK, user)
 }
+
 func DeleteUser(c echo.Context) error {
-    return c.String(http.StatusOK, "Users deleted")
+    db := d.DB
+    user := new(m.User)
+
+    if res := db.First(user, c.Param("id")); res.RecordNotFound() {
+        return c.JSON(http.StatusInternalServerError, M{
+            "Message": "User not found",
+        })
+    }
+
+    db.Delete(user)
+
+    return c.JSON(http.StatusOK, user)
 }
