4d417fa29083078e204801ceca3025ae1ab986e5
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-07-27 21:48:45 +0800

    Hash user password before saving

--- models/models.go
+++ models/models.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+    "golang.org/x/crypto/bcrypt"
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -10,4 +11,14 @@ type User struct {
     Email    string `gorm:"UNIQUE"`
     Username string
     Password string
+}
+func (u *User) BeforeSave() error {
+    hashed, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+    if err != nil {
+        return err
+    }
+
+    u.Password = string(hashed)
+
+    return nil
 }
