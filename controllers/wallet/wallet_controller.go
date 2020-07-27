941f3fe4414a58e14d2956532443edd3624ef5e7
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-07-27 22:18:25 +0800

    Complete wallet controller logic

--- controllers/wallet/wallet_controller.go
+++ controllers/wallet/wallet_controller.go
@@ -3,24 +3,79 @@ package wallet_controller
 import (
     "net/http"
     "github.com/labstack/echo/v4"
+    d "money-management-api/database"
+    m "money-management-api/models"
 )
 
+type M map[string]interface{}
+
 func FetchWallets(c echo.Context) error {
-    return c.String(http.StatusOK, "Wallets fetched")
+    db := d.DB
+    wallets := new([]m.Wallet)
+
+    db.Find(wallets)
+
+    return c.JSON(http.StatusOK, wallets)
 }
 
 func FindWallet(c echo.Context) error {
-    return c.String(http.StatusOK, "Wallets found")
+    db := d.DB
+    wallet := new(m.Wallet)
+
+    if res := db.First(wallet, c.Param("id")); res.RecordNotFound() {
+        return c.JSON(http.StatusInternalServerError, M{
+            "Message": "Wallet not found",
+        })
+    }
+
+    return c.JSON(http.StatusOK, wallet)
 }
 
 func CreateWallet(c echo.Context) error {
-    return c.String(http.StatusOK, "Wallets created")
+    db := d.DB
+    wallet := new(m.Wallet)
+
+    if err := c.Bind(wallet); err != nil {
+        panic(err.Error())
+    }
+
+    if res := db.Create(wallet); res.Error != nil {
+        return c.JSON(http.StatusInternalServerError, res.Error)
+    }
+
+    return c.JSON(http.StatusOK, wallet)
 }
 
 func UpdateWallet(c echo.Context) error {
-    return c.String(http.StatusOK, "Wallets updated")
+    db := d.DB
+    wallet := new(m.Wallet)
+
+    if res := db.First(wallet, c.Param("id")); res.RecordNotFound() {
+        return c.JSON(http.StatusInternalServerError, M{
+            "Message": "Wallet not found",
+        })
+    }
+
+    if err := c.Bind(wallet); err != nil {
+        panic(err.Error())
+    }
+
+    db.Save(wallet)
+
+    return c.JSON(http.StatusOK, wallet)
 }
 
 func DeleteWallet(c echo.Context) error {
-    return c.String(http.StatusOK, "Wallets deleted")
+    db := d.DB
+    wallet := new(m.Wallet)
+
+    if res := db.First(wallet, c.Param("id")); res.RecordNotFound() {
+        return c.JSON(http.StatusInternalServerError, M{
+            "Message": "Wallet not found",
+        })
+    }
+
+    db.Delete(wallet)
+
+    return c.JSON(http.StatusOK, wallet)
 }
