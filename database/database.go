e765fb43a8783f882f266d56b6b7b17672e462d8
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-07-27 20:42:02 +0800

    Add connection config to database

---
+++ database/database.go
@@ -0,0 +1,8 @@
+package database
+
+import (
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var DB *gorm.DB
--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+    "fmt"
     "net/http"
+    "money-management-api/database"
     "github.com/labstack/echo/v4"
     "github.com/labstack/echo/v4/middleware"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
     e := echo.New()
+
+    // Database connection
+    var err error
+    database.DB, err = gorm.Open("mysql", "root:@/db_money_management?charset=utf8&parseTime=True&loc=Local")
+    if err != nil {
+        panic("MySQL refuse to connect with us :<")
+    }
+
+    fmt.Println("MySQL already connected with us :>")
+
+    defer database.DB.Close()
 
     // Middlewares
     e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
